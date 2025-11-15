4c84040f5dd93b70ea84b918fc773f97133696db
Omri Fridman <fomri2009@gmail.com>
2025-11-15 18:05:56 +0200

    added pop back

--- Project3/Vector.cpp
+++ Project3/Vector.cpp
@@ -19,4 +19,27 @@ int Vector::capacity() const {
 	return _capacity;
 }
 
+int Vector::resizeFactor() const {
+	return _resizeFactor;
+}
 
+bool Vector::empty() const {
+	return _size == 0;
+}
+
+void Vector::push_back(const int& val) {
+	if (_size == _capacity) {
+		reserve(_capacity * _resizeFactor);
+	}
+
+	_elements[_size] = val;
+	_size++;
+}
+
+int Vector::pop_back() {
+	if (empty()) {
+		std::cout << "error: pop from empty vector" << std::endl;
+		return -9999;
+	}
+
+}
